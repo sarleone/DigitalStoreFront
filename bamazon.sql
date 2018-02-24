69bafdae9636bcbc8a5948576bdab6f41016d8e2
sarleone <sarah.m.leone@gmail.com>
2018-02-24 14:55:46 -0600

    removing node modules

--- bamazon.sql
+++ bamazon.sql
@@ -9,4 +9,14 @@ CREATE TABLE products(
     price DECIMAL(10,3) NULL,
     stock_quantity INTEGER (10) NULL,
     PRIMARY KEY(item_id)
-	);
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES
+    ('cat food', 'pets', .75, 45),
+    ('cat litter', 'pets', 4.25, 20),
+    ('banana', 'grocery', .29, 40),
+    ('goldfish', 'grocery', 1.79, 25),
+    ('dish soap', 'cleaning', 2.39, 20),
+    ('mop', 'cleaning', 5.69, 15),
+    
